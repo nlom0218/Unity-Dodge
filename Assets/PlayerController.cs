d6cabdcb28280c2e21706e5ca6ee26b02402bc65
nlom0218 <nlom0218@naver.com>
2021-12-08 22:02:50 +0900

    sdfs

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public Rigidbody playerRigidbody;
+  public float speed = 8f;
+  void Start()
+  {
+
+  }
+  void Update()
+  {
+
+  }
+  public void Die()
+  {
+    gameObject.SetActive(false);
+
+  }
+}
